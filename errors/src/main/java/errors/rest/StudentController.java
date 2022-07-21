49c1003567dfc4a2ec460abeafa8679ad5485afe
joro1002 <joro1002@abv.bg>
2022-07-21 11:34:38 +0300

    First commit

--- errors/src/main/java/errors/rest/StudentController.java
+++ errors/src/main/java/errors/rest/StudentController.java
@@ -1,2 +1,16 @@
-package errors.rest;public class StudentController {
+package errors.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    @GetMapping("/student")
+    public ResponseEntity<Student> getStudent(){
+        //arrays
+        throw new ArrayIndexOutOfBoundsException();
+    }
 }

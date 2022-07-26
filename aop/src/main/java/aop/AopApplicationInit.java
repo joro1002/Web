b5d5b52c0380317652eeb7fcb246e73a4897085c
joro1002 <joro1002@abv.bg>
2022-07-26 14:05:47 +0300

    next samples

--- aop/src/main/java/aop/AopApplicationInit.java
+++ aop/src/main/java/aop/AopApplicationInit.java
@@ -14,7 +14,7 @@ public class AopApplicationInit implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        student.sayHello();
+       // student.sayHello();
         student.echo("123");
     }
 }
